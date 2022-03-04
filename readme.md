a350f7a37601eb93546e84f26e4bbe692bbf135e
William Machado <dohillo@gmail.com>
2022-03-04 16:19:05 -0300

    configurado web pack no projeto

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Passos para rodar o projeto:
+
+- instalar todas as dependências com `npm i`
+- instalar webpack globalmente com `npm i -g webpack@5.68.0`
+- Rodar localmente usando `npm run dev`
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const res = require('express/lib/response')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World 2 3 4 5')
 })
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
