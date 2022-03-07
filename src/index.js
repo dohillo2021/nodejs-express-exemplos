a5bc28b7e6a2852b60a60f279b77c23caa0655f0
William Machado <dohillo@gmail.com>
2022-03-07 17:30:04 -0300

    Criado Rotas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import * as express from 'express'
 const { listen } = require('express/lib/application')
 const res = require('express/lib/response')
 
@@ -6,7 +6,23 @@ const res = require('express/lib/response')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World 2 3 4 5')
+    res.send('GET /')
+})
+
+app.post('/', (req, res) => {
+    res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+    res.status(400).send('PUT /')
+})
+
+app.delete('/', (req, res) => {
+    res.send('DELETE /')
+})
+
+app.patch('/', (req, res) => {
+    res.send('PATCH/')
 })
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
