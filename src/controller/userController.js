fd6656944499c4c4fd9c27a233d555a4bdeabd84
William Machado <dohillo@gmail.com>
2022-03-07 18:13:55 -0300

    Criado controles para user e post

---
+++ src/controller/userController.js
@@ -0,0 +1,14 @@
+import {Router} from 'express'
+
+const router = Router()
+router.post('/signup', (req, res) => {
+  res.send('SIGNUP /')
+})
+
+router.post('/login', (req, res) => {
+  res.send('LOGIN /')
+})
+
+
+export default router
+
--- src/index.js
+++ src/index.js
@@ -5,24 +5,12 @@ const res = require('express/lib/response')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('GET /')
-})
+import userController from './controller/userController'
 
-app.post('/', (req, res) => {
-    res.status(201).send('POST /')
-})
+import postController from './controller/postController'
 
-app.put('/', (req, res) => {
-    res.status(400).send('PUT /')
-})
 
-app.delete('/', (req, res) => {
-    res.send('DELETE /')
-})
-
-app.patch('/', (req, res) => {
-    res.send('PATCH/')
-})
+app.use('/user', userController)
+app.use('/post', postController)
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
