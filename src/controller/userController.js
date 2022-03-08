1a7f19d1bcdd474080b63107fa26032b9e1aba11
William Machado <dohillo@gmail.com>
2022-03-08 00:46:19 -0300

    rotas

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,11 +1,11 @@
 import {Router} from 'express'
 
 const router = Router()
-app.post('/signup', (req, res) => {
+router.post('/signup', (req, res) => {
   res.send('SIGNUP /')
 })
 
-app.post('/login', (req, res) => {
+router.post('/login', (req, res) => {
   res.send('LOGIN /')
 })
 
