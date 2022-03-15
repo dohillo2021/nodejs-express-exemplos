9d7f94c4a45ec7b5702d03b5c2b18c862614a1be
William Machado <dohillo@gmail.com>
2022-03-15 10:58:45 -0300

    WEBPACKCONFIGAJUSTE

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,6 @@ const path = require('path')
 const NodemonPlugin = require('nodemon-webpack-plugin')
 const nodeExternals = require('webpack-node-externals')
 
-<<<<<<< HEAD
-
-module.exports = {
-  entry: './src/index.js',
-  target: 'node',
-  mode: 'development',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js'],
-    },
-  plugins: [
-    new NodemonPlugin()
-  ],
-  externals: [
-    nodeExternals()
-  ]
-=======
 module.exports = {
     entry: './src/index.js',
     target: 'node',
@@ -39,5 +19,4 @@ module.exports = {
     externals: [
         nodeExternals()
     ]
->>>>>>> 9dd8634b02e9b3a6b8aab0d7a7589e6d62c2b2b2
 }
