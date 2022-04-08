b193b4fcec31da9514fe35b9795900c0b80730ef
William Machado <dohillo@gmail.com>
2022-04-08 16:15:16 -0300

    Corrigido espaço na rota postcontrole

--- src/modules/post/postController.js
+++ src/modules/post/postController.js
@@ -2,21 +2,29 @@ import {Router} from 'express'
 
 import { verifyAccessToken } from '../../utils/auth'
 
-import { createPost } from './postService'
+import { createPost, getPosts } from './postService'
 
 const router = Router()
 
-router.post('/ ', verifyAccessToken, (req, res) => {
+router.post('/', verifyAccessToken, (req, res) => {
   try {
-    createPost(req.body, req.user)
+    const post = createPost(req.body, req.user)
+    res.status(200).send(post)
   } catch (err) {
     res.status(500).send(err.message)
   }
 })
 
 router.get('/:id?', verifyAccessToken, (req, res) => {
-  // Duas opções: listar todos ou apenas um
-})
-
-
+    try {
+      const posts = getPosts(req.params.id)
+      res.status(200).send(posts)
+    } catch (err) {
+      if (err.message === 'post_nao_existe')
+        return res.status(400).send(err.message)
+      
+      return res.status(500).send(err.message)
+    }  
+  })
+  
 export default router
--- src/modules/post/postService.js
+++ src/modules/post/postService.js
@@ -1,12 +1,20 @@
 let posts = []
 
 export const createPost = (data, user) => {
-    console.log({
-        data,
-        user
-    })
-    posts.push({ 
+    
+    const post ={
         createdBy: user.email,
         ...data
-    })
+    }
+    posts.push(post)
+    return post
+}
+
+export const getPosts = (id) => {
+    if(id) {
+        const post = posts[id]
+        if (!post) throw new Error('post_nao_existe')
+        return post
+       }
+    return posts
 }
